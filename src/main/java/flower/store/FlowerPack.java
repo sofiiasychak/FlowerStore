4ec288d3fa2ce3a605768f68d9567b9e2ab4a19f
sofiiasychak <sychak.pn@ucu.edu.ua>
2025-10-11 19:47:01 +0300

    Update FlowerPack.java

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -1,6 +1,14 @@
 package flower.store;
 
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+@Getter
 public class FlowerPack {
     private Flower flower;
     private int quantity;
+    
+    public double getPrice() {
+        return flower.getPrice() * quantity;
+    }
 }
