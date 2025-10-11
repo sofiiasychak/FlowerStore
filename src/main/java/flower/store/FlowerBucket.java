790c9b90a4054e558312aad2b82dc01fcc8cff7f
sofiiasychak <sychak.pn@ucu.edu.ua>
2025-10-11 19:57:53 +0300

    Update FlowerBucket.java

--- src/main/java/flower/store/FlowerBucket.java
+++ src/main/java/flower/store/FlowerBucket.java
@@ -1,1 +1,20 @@
+package flower.store;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlowerBucket {
+    private List<FlowerPack> flowerPacks = new ArrayList<>();
+    
+    public void add(FlowerPack flowerPack) {
+        flowerPacks.add(flowerPack);
+    }
+    
+    public double getPrice() {
+        double totalPrice = 0;
+        for (FlowerPack pack : flowerPacks) {
+            totalPrice += pack.getPrice();
+        }
+        return totalPrice;
+    }
+}
