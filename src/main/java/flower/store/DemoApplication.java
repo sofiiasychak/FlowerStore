5c65a43917487c89710087e5e5bc5a2affd59e44
Sofiia Sychak <sychak.pn@ucu.edu.ua>
2025-10-27 21:18:10 +0200

    Added changes to coonet to springboot

---
+++ src/main/java/flower/store/DemoApplication.java
@@ -0,0 +1,12 @@
+package flower.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("DemoApplication started (Spring Boot not required on the classpath).");
+    }
+}
